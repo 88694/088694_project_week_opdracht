c9cbe9465a26b6cb5f4e5956a28cbdc24110372f
88694 <88694@glr.nl>
2023-09-12 11:17:37 +0100

    v.1.0 first late commit

---
+++ contact.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Projectweek</title>
+    <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/CSS/index.css">
+</head>
+
+<body>
+<div class="drop-down">
+    <div class="navbar">
+        <ul>
+            <li> <a href="index.html">Home</a></li>
+            <li> <a>About</a>
+                <ul>
+                    <li><a href=CVskills.html">CV/Skill</a></li>
+                    <li><a href="artist.html">Artist</a></li>
+                    <li><a href="project.html">Projects</a></li>
+                    </li>
+                </ul>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+</div>
+<div class="container-box">
+
+    <div class="main-contact">
+       
+    <form method="POST">
+            <h1>Contact Us</h1>
+            <div class="info">
+                <input class="fname" type="text" name="name" placeholder="Full name">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="phone" placeholder="Phone number">
+                <input type="text" name="company" placeholder="Company name">
+            </div>
+            <p>Message</p>
+            <div>
+                <textarea rows="4" name="message"></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+   
+    </div>
+</div>
+</body>
+
+</html>
+
+
+
+
+
+<?php
+$host = 'localhost:3306';
+$dbUsername = "88694";
+$dbPassword = "AAPJE123!";
+$dbName = "databaseprojectweek1";
+
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $company = $_POST["company"];
+    $message = $_POST["message"];
+
+    $sql = "INSERT INTO Portfolio (naam, email, nummer, bedrijf, vraag) VALUES ('$name', '$email', '$phone', '$company', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
